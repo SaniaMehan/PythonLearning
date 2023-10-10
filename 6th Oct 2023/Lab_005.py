4024c68dcfd70c31eb74b49ba3de08cc349ddcf2
SaniaMehan <saniagumber777@gmail.com>
2023-10-10 16:05:40 +0530

    commit

---
+++ 6th Oct 2023/Lab_005.py
@@ -0,0 +1,16 @@
+# slice = division of characters
+name = "sanjay"
+print(name)
+# slice
+#Means start from zero & till 2 characters
+print(name[0:2])
+print(name[0:3])
+print(name[0:6])
+# Doesn't give error when limit cross of characters
+print(name[0:7])
+
+print("-------")
+
+#Means starts from 2 & give the 3th character(3 is the length of character)
+#print(name[2:length-1])
+print(name[2:3])
---
+++ 6th Oct 2023/Lab_007.py
@@ -0,0 +1,5 @@
+print('C:\some\name')
+# in above \n understand new line so that use r in below means raw
+print(r'C:\some\name')
+random_string = "I am Batman"
+print(len(random_string))
